2b49113397ab409fa37522942dc92d4ffe6461e7
付杰 <fujie@sinosoft.com.cn>
2021-07-11 16:28:25 +0800

    去哪儿网

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <keep-alive exclude="Detail">
+      <router-view/>
+    </keep-alive>
+  </div>
+</template>
+
+
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import store from './store'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
+
+Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
+
+//**** 本段代码做了变更 ****
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
